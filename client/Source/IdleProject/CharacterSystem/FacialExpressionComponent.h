26fcdbafdeb7e92c3e54b0ab75ae4f4ade26b7f8
ewoo14 <ewoo2821@gmail.com>
2026-05-25 19:32:04 +0900

    codex(character): add facial expression component

---
+++ client/Source/IdleProject/CharacterSystem/FacialExpressionComponent.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "FacialExpressionComponent.generated.h"
+
+class USkeletalMeshComponent;
+
+UENUM(BlueprintType)
+enum class EFacialExpression : uint8
+{
+	None = 0 UMETA(Hidden),
+	Idle = 1 UMETA(DisplayName = "Idle"),
+	Battle = 2 UMETA(DisplayName = "Battle"),
+	Smile = 3 UMETA(DisplayName = "Smile"),
+	Hit = 4 UMETA(DisplayName = "Hit"),
+	Death = 5 UMETA(DisplayName = "Death"),
+	LevelUp = 6 UMETA(DisplayName = "LevelUp")
+};
+
+/** VRoid/VRM4U Morph Target 이름에 맞춰 캐릭터 표정을 제어합니다. */
+UCLASS(ClassGroup = (Idle), meta = (BlueprintSpawnableComponent))
+class IDLEPROJECT_API UFacialExpressionComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UFacialExpressionComponent();
+
+	UFUNCTION(BlueprintCallable, Category = "Idle|Facial")
+	void SetExpression(EFacialExpression NewExpression, float Duration = -1.0f);
+
+	UFUNCTION(BlueprintCallable, Category = "Idle|Facial")
+	EFacialExpression GetCurrentExpression() const { return CurrentExpression; }
+
+protected:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Idle|Facial")
+	EFacialExpression CurrentExpression = EFacialExpression::Idle;
+
+	FTimerHandle RevertTimerHandle;
+
+	USkeletalMeshComponent* GetTargetMesh() const;
+	void ApplyBlendShapes(EFacialExpression Expression);
+	void RevertToIdle();
+};
