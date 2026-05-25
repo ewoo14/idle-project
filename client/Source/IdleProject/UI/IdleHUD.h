af495863e5cc32ffc293f5ec2f6e641762221f32
ewoo14 <ewoo2821@gmail.com>
2026-05-25 16:17:34 +0900

    codex(designer): IdleHUDWidget Slate UI 실구현 + 테마 토큰 적용

--- client/Source/IdleProject/UI/IdleHUD.h
+++ client/Source/IdleProject/UI/IdleHUD.h
@@ -5,7 +5,8 @@
 #include "IdleHUD.generated.h"
 
 class UIdleGameInstance;
-class SWidget;
+class UCombatComponent;
+class SIdleHUDWidget;
 
 /** Slate HUD 구현을 붙이기 위한 최소 AHUD 베이스입니다. */
 UCLASS()
@@ -15,6 +16,8 @@ class IDLEPROJECT_API AIdleHUD : public AHUD
 
 public:
 	virtual void PostInitializeComponents() override;
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
 protected:
 	UFUNCTION()
@@ -23,9 +26,20 @@ protected:
 	UFUNCTION()
 	void HandleExpChanged(int64 CurrentExp, int64 NextExp);
 
+	UFUNCTION()
+	void HandleLevelUp(int32 NewLevel);
+
+	UFUNCTION()
+	void HandleHpChanged(float NewHp);
+
 private:
+	void BindPlayerCombat();
+
 	UPROPERTY(Transient)
 	TObjectPtr<UIdleGameInstance> IdleGameInstance;
 
-	TSharedPtr<SWidget> RootWidget;
+	UPROPERTY(Transient)
+	TObjectPtr<UCombatComponent> PlayerCombat;
+
+	TSharedPtr<SIdleHUDWidget> RootWidget;
 };
